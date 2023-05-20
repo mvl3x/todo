2a2c37c1a52cfd9a575584c41a4af28c906fd08e
mvl3x <125395126+mvl3x@users.noreply.github.com>
2023-05-20 17:59:04 +0500

    refactor: split "el" to components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const el = (
-  <div>
-    <h1>My Todo List</h1>
-    <input placeholder="search" />
+const TodoList = () => {
+  return (
     <ul>
       <li>Learn React</li>
       <li>Build Awesome App</li>
     </ul>
+  );
+};
+const AppHeader = () => {
+  return <h1>My Todo List</h1>;
+};
+const SearchPanel = () => {
+  return <input placeholder="search" />;
+};
+
+const App = () => (
+  <div>
+    <AppHeader />
+    <SearchPanel />
+    <TodoList />
   </div>
 );
 
-ReactDOM.render(el, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
