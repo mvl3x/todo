4ff397ee9df43c3a30c4fd7d476ca11ef55b87f2
mvl3x <125395126+mvl3x@users.noreply.github.com>
2023-05-22 13:57:50 +0500

    refactor: use createRoot() for render

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 import App from "./components/app/app";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
